90ae2810cd3584ec2abc47d7b3cd0ad608ca2cec
Michael Brooks <me@michaelbrooks.co.uk>
2020-06-29 09:41:36 +0100

    Added custom 404

---
+++ src/pages/404.vue
@@ -0,0 +1,19 @@
+<template>
+    <Layout :show-logo="false" class="bg-black">
+        <div class="container mx-auto text-center md:my-64 mb-64 px-2 md:px-0">
+            <h1 class="text-white md:text-5xl">
+                This page seems to have been lost in the forest.
+            </h1>
+            <h1 class="text-white md:text-5xl mt-6">
+                We will continue searching through the trees.
+            </h1>
+            <h1 class="text-white md:text-5xl mt-6">
+                In the meantime, please try using the search or links above.
+            </h1>
+        </div>
+    </Layout>
+</template>
+
+<script>
+
+</script>
